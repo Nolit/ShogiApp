0c26499da8d70ecf4ee3cc97d07b1163ba6bcc7e
Nolit <yaboonjapan@gmail.com>
2016-05-17 22:51:04 +0900

    Mainctivityを移行 Login・MyPage・Loadinクラスはモック

---
+++ app/src/main/java/com/uty/shogi/activities/Matching.java
@@ -0,0 +1,32 @@
+package com.uty.shogi.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.uty.shogi.R;
+//これはJavaからKotlin移行時のモッククラスです
+//プロジェクトには必要ありません
+public class Matching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
